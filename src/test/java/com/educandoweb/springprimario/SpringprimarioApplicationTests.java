90c11ced47c98135b8c2dc9d86ab63f1e990d3d8
dev.gomes <devgomes07@gmail.com>
2026-04-12 00:54:51 -0300

    feat: primeiro commit do projeto

---
+++ src/test/java/com/educandoweb/springprimario/SpringprimarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springprimario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprimarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
