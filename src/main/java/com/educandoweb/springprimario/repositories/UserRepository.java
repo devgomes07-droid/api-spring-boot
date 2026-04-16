fc1e80d28031808e49d70bf3f8e4c15c4bdf8227
dev.gomes <devgomes07@gmail.com>
2026-04-15 23:11:17 -0300

    feat: configuração do H2 com perfil de teste e carga inicial de usuários

--- src/main/java/com/educandoweb/springprimario/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/springprimario/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.educandoweb.springprimario.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
